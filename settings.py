cf6e31900ab1ad14f392fc54637e637632dfbf73
Kirill Kuzin <offkirillkuzin@gmail.com>
2019-03-08 21:31:57 +0300

    Removed bot token cast to string.

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 import os
 
 DEBUG_MODE = os.environ.get('DEBUG_MODE')
-BOT_TOKEN = str(os.environ.get('BOT_TOKEN'))
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
 INFURA_TOKEN = os.environ.get('INFURA_TOKEN')
 WEBHOOK_LINK = os.environ.get('WEBHOOK_LINK')
