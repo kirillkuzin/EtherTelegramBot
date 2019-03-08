40a906f1430b619730434531bfa74378188cc6d4
Kirill Kuzin <offkirillkuzin@gmail.com>
2019-03-08 22:13:52 +0300

    Work with confirmation custom transactions.
    Import ethereum module.
    Import redis module.
    Added function for inline callback for confirmation custom transaction.
    Updated API function (tx) for accept custom transactions.

--- application.py
+++ application.py
@@ -1,16 +1,28 @@
 import telebot
 import os
-from settings import *
+from settings import BOT_TOKEN, WEBHOOK_LINK, DEBUG_MODE
 from flask import Flask, request
+from ethereum_core import Ethereum
+from redis_core import Redis
 
 bot = telebot.TeleBot(BOT_TOKEN)
 application = Flask(__name__)
+ethereum = Ethereum()
+redis = Redis()
 
 chatIds = {}
 
 @bot.message_handler(commands = ['start'])
 def startMessage(message):
     bot.send_message(message.chat.id, 'Start')
+
+@bot.callback_query_handler(func = lambda call: True)
+def callback(call):
+    parsedData = call.data.split(' ')
+    status = parsedData[0]
+    txId = int(parsedData[1])
+    if status == 'confirmed':
+        tx = redis.getTx(txId)
 
 @bot.message_handler(func=lambda message: True)
 def textMessage(message):
@@ -30,10 +42,13 @@ def webhook():
 
 @application.route('/tx', methods = ['POST'])
 def tx():
-    addressFrom = request.form.get('from')
-    addressTo = request.form.get('to')
-    value = request.form.get('value')
-    bot.send_message(chatIds.get(addressFrom), addressFrom + ' ' + addressTo + ' ' + value)
+    fromAddress = str(request.form.get('from'))
+    toAddress = str(request.form.get('to'))
+    value = int(request.form.get('value'))
+    data = str(request.form.get('data'))
+    tx = ethereum.buildTransaction(fromAddress, toAddress, value)
+    txId = redis.addNewTx(tx)
+    bot.send_message(0, 'New tx:', reply_markup = acceptTxMarkup(txId))
     return '', 200
 
 if __name__ == '__main__':
